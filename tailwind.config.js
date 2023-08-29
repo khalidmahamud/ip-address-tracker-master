8910829a61d235167fb361e9388b050a29b9ace8
Khalid <mahamud.0.khalid@gmail.com>
2023-08-30 01:46:28 +0600

    completed top section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./build/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+          'Very-Dark-Gray': 'hsl(0, 0%, 17%)',
+          'Dark-Gray': 'hsl(0, 0%, 59%)',
+      },
+      fontFamily: {
+        'rubik': ['Rubik', 'sans-serif'],
+      },
+      backgroundImage: {
+        'map-pattern-desktop': "url('/build/images/pattern-bg-desktop.png')",
+        'map-pattern-mobile': "url('/build/images/pattern-bg-mobile.png')",
+      },
+    },
   },
   plugins: [],
 }
